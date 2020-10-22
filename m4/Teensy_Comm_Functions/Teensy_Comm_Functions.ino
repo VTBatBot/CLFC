b7f2cd4909e5d70b0fdbc7093ba750dca0c3e565
Patrick Braun <patdb17@vt.edu>
2020-10-21 20:00:25 -0400

    Fixed this test File
    Reads in opcode and data from M4 using UART and writes to Serial Moniter, but shows up weird on Serial Moniter

--- m4/Teensy_Comm_Functions/Teensy_Comm_Functions.ino
+++ m4/Teensy_Comm_Functions/Teensy_Comm_Functions.ino
@@ -2,17 +2,23 @@
 
 void setup() {
   Serial1.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-}
-  if Serial1.available(){
-    
+  if (Serial1.available()){
+    uint8_t motionProfile;
+    uint8_t pwmSetting; 
+     
     uint8_t opcode = Serial1.read();
     if (opcode == 0x01) {
       motionProfile = Serial1.read();
       pwmSetting = Serial1.read();
     }
-
+    Serial.write(opcode);
+    Serial.write(motionProfile);
+    Serial.write(pwmSetting);
+    
+  }
 }
+  
